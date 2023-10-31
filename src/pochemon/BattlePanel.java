12f1f16dd5c7457373e3b0773611422046e48197
Valentin Rieu <rieuvalentin3@gmail.com>
2023-10-31 17:48:57 +0100

    ajout ecran de debut combat

--- src/pochemon/BattlePanel.java
+++ src/pochemon/BattlePanel.java
@@ -6,12 +6,13 @@ import javax.imageio.*;
 import java.io.*;
 import java.awt.image.BufferedImage;
 
-public class BattlePanel extends JPanel implements Runnable {
+import pochemon.battle.Battle;
+
+public class BattlePanel extends JPanel {
     public static final int screenHeight = GamePanel.screenHeight;
     public static final int screenWidth = (int)(1.2*GamePanel.screenWidth);
-    private static final int FPS = 30;
     private final BufferedImage backgroundImage;
-    private static Thread gameThread;
+    Battle battle;
 
     public BattlePanel() {
         BufferedImage image = null;
@@ -31,45 +32,21 @@ public class BattlePanel extends JPanel implements Runnable {
         backgroundImage = image;
     }
 
-    public void startGameThread() {
-        gameThread = new Thread(this);
-        gameThread.start();
-    }
-
-    public void run() {
-        double drawInterval = 1000000000/FPS;
-        double delta = 0;
-        long lastTime = System.nanoTime();
-        long currentTime;
-        long timer = 0;
-        int drawCount = 0;
-
-        while(gameThread!=null) {
-            currentTime = System.nanoTime();
-            delta += (currentTime-lastTime)/drawInterval;
-            timer += (currentTime-lastTime);
-            lastTime = currentTime;
-
-            if(delta>=1) {
-                update();
-                repaint();
-                delta--;
-                drawCount++;
-            }
-
-            if(timer>=1000000000) {
-                drawCount = 0;
-                timer = 0;
-            }
-        }
-    }
-
-    public void update() {
+    public void addBattle(Battle battle) {
+        this.battle = battle;
     }
 
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
         Graphics2D g2 = (Graphics2D)g;
         g2.drawImage(backgroundImage, 0, 0, screenWidth, screenHeight, null);
+        battle.startAnimation(g2);
+
+        try {
+            Thread.sleep(2000);
+            //g2.drawImage(backgroundImage, 0, 0, screenWidth, screenHeight, null);
+        } catch(InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
--- src/pochemon/battle/Battle.java
+++ src/pochemon/battle/Battle.java
@@ -1,8 +1,10 @@
 package pochemon.battle;
 
-import javax.swing.JDialog;
-import javax.swing.JFrame;
-import java.awt.Dialog;
+import javax.swing.*;
+import java.awt.*;
+import javax.imageio.*;
+import java.io.*;
+import java.awt.image.BufferedImage;
 
 import pochemon.ui.entities.Player;
 import pochemon.ui.entities.Dresser;
@@ -24,10 +26,10 @@ public class Battle {
         JDialog window = new JDialog();
         window.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
         window.setResizable(false);
-        window.setTitle("Combat");
+        window.setTitle("Combat : Joueur vs "+opponent.name);
 
         BattlePanel battlePanel = new BattlePanel();
-        battlePanel.startGameThread();
+        battlePanel.addBattle(this);
         window.add(battlePanel);
         window.pack();
 
@@ -36,5 +38,30 @@ public class Battle {
         window.setModalityType(Dialog.ModalityType.APPLICATION_MODAL);
         window.setLocationRelativeTo(null);
         window.setVisible(true);
+    }
+
+    public void startAnimation(Graphics2D g) {
+        BufferedImage ryanGosling = null;
+        BufferedImage obiwan = null;
+        BufferedImage VSLogo = null;
+
+        try {
+            // Je n'ai pas reussi a trouver d'images interessantes.
+            ryanGosling = ImageIO.read(new FileInputStream("../res/sprites/ryan_gosling_cereal.png"));
+            obiwan = ImageIO.read(new FileInputStream("../res/sprites/obiwan.png"));
+            VSLogo = ImageIO.read(new FileInputStream("../res/sprites/vs_logo.png"));
+        } catch(IOException e) {
+            System.out.println("res folder was altered.");
+            System.exit(1);
+        }
+
+        int xSize = 200;
+        int ySize = 200;
+        int middleY = (int)(BattlePanel.screenHeight/2 - ySize/2);
+        int middleX = (int)(BattlePanel.screenWidth/2 - xSize/2);
+
+        g.drawImage(ryanGosling, 0, middleY, xSize, ySize, null);
+        g.drawImage(VSLogo, middleX, middleY, xSize, ySize, null);
+        g.drawImage(obiwan, BattlePanel.screenWidth-xSize, middleY, xSize, ySize, null);
     }
 }
